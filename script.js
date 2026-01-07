f281a9882f292f90f04126769792351492669ee6
Ishanth-Hombaiah <hombaiah01@gmail.com>
2026-01-06 19:03:33 -0500

    Added storing past read books functionality

--- script.js
+++ script.js
@@ -9,13 +9,20 @@ const command = process.argv[2];
 const today = new Date(); 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-if (command == "daily") {
-    const story = short_stories[Math.floor(Math.random() * short_stories.length)];
-    const weekday = days[today.getDay()];
-    const date = today.toDateString().slice(4);
-
-    console.log(`Today's date is ${date}, a ${weekday}.`);
-    console.log(`The daily selection is ${story.title} by ${story.author}.`);
-} else {
-    console.log("Apologies, this is an unfamiliar command.");
+if (state.used.length === short_stories.length) {
+    console.log("There are no stories remaining.");
+    process.exit(0);
 }
+
+do { story = short_stories[Math.floor(Math.random() * short_stories.length)]; } 
+while (state.used.includes(story.id));
+
+state.used.push(story.id);
+await writeFile("state.json", JSON.stringify(state));
+
+const story = short_stories[Math.floor(Math.random() * short_stories.length)];
+const weekday = days[today.getDay()];
+const date = today.toDateString().slice(4);
+
+console.log(`Today's date is ${date}, a ${weekday}.`);
+console.log(`The daily selection is ${story.title} by ${story.author}.`);
